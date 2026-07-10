3a71d59fafee0fd3c299044ba3658def4c9210ca
Tran Quan <quantran2k6@gmail.com>
2026-07-10 15:52:37 +0700

    no commit

--- src/main/java/com/rikkei/course141/ss1/ApiResponse.java
+++ src/main/java/com/rikkei/course141/ss1/ApiResponse.java
@@ -8,18 +8,19 @@ import lombok.*;
 @Builder
 public class ApiResponse<T> {
     private int status;
+    private Boolean success;
     private String message;
     private T data;
 
     public static <T> ApiResponse<T> success(T data) {
-        return ApiResponse.<T>builder().status(200).message("Success").data(data).build();
+        return ApiResponse.<T>builder().status(200).success(true).message("Success").data(data).build();
     }
 
     public static <T> ApiResponse<T> error(int status, String message) {
-        return ApiResponse.<T>builder().status(status).message(message).data(null).build();
+        return ApiResponse.<T>builder().status(status).success(false).message(message).data(null).build();
     }
 
     public static <T> ApiResponse<T> error(int status, String message, T data) {
-        return ApiResponse.<T>builder().status(status).message(message).data(data).build();
+        return ApiResponse.<T>builder().status(status).success(false).message(message).data(data).build();
     }
 }
